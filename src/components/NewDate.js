82089de3b46772517ba62bb05196ddb90ed7f8ef
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-08-11 18:38:35 +0430

    added contact form

--- src/components/NewDate.js
+++ src/components/NewDate.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function NewDate(props) {
   return (
     <div style={{ fontSize: "20px", color: " #ebf6f6", textAlign: "center" }}>
-      {props.date.getFullYear()}
+      <p>
+        <span style={{ fontSize: "25px" }}>&copy;</span>{" "}
+        {props.date.getFullYear()}
+      </p>
     </div>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import "./Contact.css";
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      {" "}
+      <Navbar />
+      <section class="contact-section wrapper ">
+        <h2>Contact Me</h2>
+        <form
+          action="https://formspree.io/f/mrblkobl"
+          method="POST"
+          class="contact-form"
+        >
+          <label for="name">Name</label>
+          <input type="text" id="name" name="name" placeholder="Your name" />
+
+          <label for="email">
+            Email<span>*</span>
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Your email"
+            required
+          />
+
+          <label for="message">
+            Message<span>*</span>
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            placeholder="Write your message here..."
+            required
+          ></textarea>
+
+          <button type="submit">Send Message</button>
+        </form>
+      </section>
+    </div>
+  );
 }
