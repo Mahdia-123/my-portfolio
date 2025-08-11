d284c3436fe403cb8396acf5cf89d9a762743e0c
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-08-11 11:10:00 +0430

    added the content of about page

--- src/Component/NewDate.js
+++ src/Component/NewDate.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function NewDate(props) {
   return (
-    <div style={{ fontSize: "30px", color: " #ebf6f6", textAlign: "center" }}>
+    <div style={{ fontSize: "20px", color: " #ebf6f6", textAlign: "center" }}>
       {props.date.getFullYear()}
     </div>
   );
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,82 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
 import Navbar from "../Component/Navbar";
 import Footer from "../Component/Footer";
+import "./About.css";
+import "../Component/Footer.css";
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      {" "}
+      <section class="about-section-fluid grid grid-2-columns">
+        <div>
+          <h1 class="mb-3">Hello, I'm Mahdia Khamoosh</h1>
+          <h3 class="mb-3">Front-end web developer based in Afghanistan</h3>
+          <p class="mb-5">
+            I’m currently seeking new opportunities in front-end development.
+            I’m open to work remotely and have flexible hours. I’m highly
+            skilled in JavaScript and React.
+          </p>
+
+          <a href="projects.html" rel="noreferrer">
+            See my Projects
+          </a>
+        </div>
+        <div>
+          <img
+            src="images/mahdia.JPEG"
+            alt="mahdia-khamoosh"
+            class="img-fluid"
+          />
+        </div>
+      </section>
+      <section class="about-section-fluid-skills">
+        <h1 class="text-center">My Skills</h1>
+        <div class="my-skills grid grid-3-columns mt-5 text-center">
+          <div>
+            <i class="fa-brands fa-html5"></i>
+            <div class="skills-title">Html</div>
+          </div>
+          <div>
+            <i class="fa-brands fa-css3"></i>
+            <div class="skills-title">Css</div>
+          </div>
+          <div>
+            <i class="fa-brands fa-java"></i>
+            <div class="skills-title">Javascript</div>
+          </div>
+          <div>
+            <i class="fa-brands fa-react"></i>
+            <div class="skills-title">React.js</div>
+          </div>
+          <div>
+            <i class="fa-brands fa-bootstrap"></i>
+            <div class="skills-title">Bootstrap</div>
+          </div>
+          <div>
+            <i class="fa-solid fa-mobile-screen-button"></i>
+            <div class="skills-title">Responsive</div>
+          </div>
+          <div>
+            <i class="fa-brands fa-git-alt"></i>
+            <div class="skills-title">Git</div>
+          </div>
+
+          <div>
+            <i class="fa-brands fa-github"></i>
+            <div class="skills-title">Github</div>
+          </div>
+          <div>
+            <i class="fa-solid fa-laptop-code"></i>
+            <div class="skills-title">VSCode</div>
+          </div>
+        </div>
+        <p class="tech text-center mt-5">
+          <strong>Technologies: </strong> HTML, CSS, JavaScript, Bootstrap
+        </p>
+      </section>
+      <Footer />
+    </div>
+  );
 }
