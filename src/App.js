47e0cc1b1d62c45433338449c92833981417b8c1
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-08-11 09:17:30 +0430

    added more links

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Navbar from "./Component/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-
 import Achivements from "./pages/Achivements";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
@@ -14,6 +15,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/achivements" element={<Achivements />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
